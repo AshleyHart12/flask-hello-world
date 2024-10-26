adb4486ebf022437c3df85bf993eb800e4b09a09
AshleyHart12 <ahart425@gmail.com>
2024-10-26 01:19:30 +0000

    adding create route

--- app.py
+++ app.py
@@ -12,3 +12,19 @@ def deb_test():
     conn = psycopg2.connect("postgresql://flask_database_24bv_user:3qisfEWiB1nELMoC8SXlOhCyQXVG6JaG@dpg-cse42qu8ii6s738sfqs0-a/flask_database_24bv")
     conn.close()
     return "DB Test Working"
+
+@app.route('/db_create')
+    conn = psycopg2.connect("postgresql://flask_database_24bv_user:3qisfEWiB1nELMoC8SXlOhCyQXVG6JaG@dpg-cse42qu8ii6s738sfqs0-a/flask_database_24bv")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    conn.commit()
+    conn.close()
+    return "basketball successful"
