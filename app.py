1b39caaeeb5fc206c3ebc0c076362fac3e61c519
AshleyHart12 <ahart425@gmail.com>
2024-10-26 01:25:37 +0000

    fixing create error

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ def hello_world():
     return 'Hello World from Ashley Judah in 3308!'
 
 @app.route('/db_test')
-def deb_test():
+def db_test():
     conn = psycopg2.connect("postgresql://flask_database_24bv_user:3qisfEWiB1nELMoC8SXlOhCyQXVG6JaG@dpg-cse42qu8ii6s738sfqs0-a/flask_database_24bv")
     conn.close()
     return "DB Test Working"
 
 @app.route('/db_create')
+def db_create():
     conn = psycopg2.connect("postgresql://flask_database_24bv_user:3qisfEWiB1nELMoC8SXlOhCyQXVG6JaG@dpg-cse42qu8ii6s738sfqs0-a/flask_database_24bv")
     cur = conn.cursor()
     cur.execute('''
